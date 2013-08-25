b7b671f0b61bcf7ce6920b24a3469878ea41c4f9
dalcde <dalcde@yahoo.com.hk>
2013-08-25 18:34:43 +0800

    Remove unused files

--- settings-modules/homepage/page.php
+++
@@ -1,9 +0,0 @@
-<?php
-$PAGES=['save-file',
-        'add-student-record',
-        'time-property',
-        'user-accounts'];
-foreach ($PAGES as $PAGE) {
- echo "<a href='?p=settings&mod=".$PAGE."'>".$PAGE."</a><br />";
-}
-?>
