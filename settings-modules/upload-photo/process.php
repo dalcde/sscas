6cc437f375ecd3a25d86318abaab5e726a9afa58
dalcde <dalcde@yahoo.com.hk>
2013-09-15 18:40:07 +0800

    [Upload Photos] Only extract JPEG files

--- settings-modules/upload-photo/process.php
+++ settings-modules/upload-photo/process.php
@@ -10,27 +10,40 @@ if ($_FILES["photo-file"]["name"]){
 
     $accepted_types = array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed');
     foreach( $accepted_types as $mime_type) {
-	if( $mime_type == $type ) {
-	    $okay = true;
-	    break;
-	} 
+        if( $mime_type == $type ) {
+            $okay = true;
+            break;
+        } 
     }
 
     if (strtolower($name[1]) != "zip") {
-	$okay = false;
+        $okay = false;
     }
 
     if ($okay) {
-	$zip = new ZipArchive();
-	if ($zip->open($source)) {
-	    $zip->extractTo("../../photos/");
-	    $zip->close();
-	    echo "Success!";
-	} else {
-	    echo "Unable to extract archive";
-	}
+        $zip = new ZipArchive();
+        if ($zip->open($source)) {
+            mkdir("/tmp/photos");
+            $zip->extractTo("/tmp/photos");
+            $zip->close();
+
+            if ($handle = opendir('/tmp/photos')) {
+                while (false !== ($fileName = readdir($handle))) {
+                    $arr = explode(".", $fileName);
+                    if ($arr[count($arr)] != "jpg" && $arr[count($arr)-1] != "JPG") {
+                        continue;
+                    }
+                    rename("/tmp/photos/".$fileName, "../../photos/".$fileName);
+                }
+                closedir($handle);
+            }
+
+            echo "Success!";
+        } else {
+            echo "Unable to extract archive";
+        }
     } else {
-	echo "Uplaoded file is not zip archive";
+        echo "Uplaoded file is not zip archive";
     }
 }
 ?>
