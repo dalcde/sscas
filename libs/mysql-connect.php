773b6a0d083ec16abc993553944a28810161f48b
dalcde <dalcde@yahoo.com.hk>
2013-10-14 18:28:57 +0800

    Generate random MySQL password on setup

--- libs/mysql-connect.php
+++ libs/mysql-connect.php
@@ -1,12 +1,19 @@
 <?php
+if (!$path) {
+  $path = "etc/passwords";
+}
+$file = fopen($path, "r");
+$sscas_password = trim(fgets($file));
+$sscas_admin_password = trim(fgets($file));
+fclose($file);
 
 $HOSTNAME="localhost";
 $USERNAME="sscas";
-$PASSWORD="sscas";
+$PASSWORD=$sscas_password;
 
 if ($_SESSION["loggedin"] && $_SESSION["type"] == 0) {
   $USERNAME="sscas_admin";
-  $PASSWORD="sscas_admin";
+  $PASSWORD=$sscas_admin_password;
 }
 
 $connection = mysql_connect($HOSTNAME,$USERNAME,$PASSWORD);
--- setup-process.php
+++ setup-process.php
@@ -20,6 +20,15 @@ if (strcmp($_POST["admin-pass"], $_POST["retype-pass"]) != 0) {
 
 $password_hash = hash("sha256", $_POST["admin-pass"]);
 
+$sscas_password = substr(str_shuffle(str_repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',5)),0,8); // Random string
+$sscas_admin_password = substr(str_shuffle(str_repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',5)),0,8);
+
+$file = fopen("etc/passwords", "w");
+fwrite($file, $sscas_password);
+fwrite($file, "\n");
+fwrite($file, $sscas_admin_password);
+fclose($file);
+
 mysql_query("CREATE DATABASE sscas CHARACTER SET utf8 COLLATE utf8_bin");
 mysql_query("CREATE DATABASE save_files CHARACTER SET utf8 COLLATE utf8_bin");
 mysql_query("CREATE DATABASE student_records CHARACTER SET utf8 COLLATE utf8_bin");
@@ -32,8 +41,8 @@ mysql_query("CREATE TABLE sscas.config(save_file_table VARCHAR(50))");
 mysql_query("INSERT INTO sscas.logins VALUES ('admin', '$password_hash', 0)");
 mysql_query("INSERT INTO sscas.config VALUES ('')");
 
-mysql_query("CREATE USER 'sscas'@'localhost' IDENTIFIED BY 'sscas'");
-mysql_query("CREATE USER 'sscas_admin'@'localhost' IDENTIFIED BY 'sscas_admin'");
+mysql_query("CREATE USER 'sscas'@'localhost' IDENTIFIED BY '$sscas_password'");
+mysql_query("CREATE USER 'sscas_admin'@'localhost' IDENTIFIED BY '$sscas_admin_password'");
 
 mysql_query("GRANT SELECT on sscas.* TO 'sscas'@'localhost'");
 mysql_query("GRANT SELECT,UPDATE on save_files.* TO 'sscas'@'localhost'");
